f37aee11f7eab4b5fe99fedcbb1b66976608a0f2
Aymane <elachabaymane995@gmail.com>
2023-09-07 00:08:44 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string to the stdout
+ *
+ * Return: 0 for Success
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
