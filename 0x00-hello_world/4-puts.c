1fed2e996fdc54373ddcef27bcb2e4377d9ea03e
Aymane <elachabaymane995@gmail.com>
2023-09-06 23:53:30 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * Prints a string to the stdout.
- */
-
+ * main - Prints a string to the stdout.
+ *
+ * Return: Always 0
+*/
 int main(void)
 {
-	char str[51] = "Programming is like building a multilingual puzzle";
+	char str[52] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
