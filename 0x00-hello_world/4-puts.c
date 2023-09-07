7b87e7dd5ff6b97f0a84b223b64c25225ed0074d
Aymane <elachabaymane995@gmail.com>
2023-09-06 23:50:04 -0700

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Prints a string to the stdout.
+ */
+
+int main(void)
+{
+	char str[51] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
