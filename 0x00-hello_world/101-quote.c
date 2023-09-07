d0359eec36b769d0290ff45d125e52ee8983d4c4
Aymane <elachabaymane995@gmail.com>
2023-09-07 04:32:53 -0700

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text to the stdout
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", stderr);
+	return (1);
+}
+
