f8d4adeb3939b991d5f56c59fc8098d15fbe4859
Aymane <elachabaymane995@gmail.com>
2023-09-08 13:02:50 -0700

    combo of three

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return 0
+ */
+int main(void)
+{
+	int i;
+
+	int j;
+
+	int z = 0;
+
+	while (z < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			i = 0;
+			while (i < 10)
+			{
+				if (i != j && j != z && z < j && j < i)
+				{
+					putchar('0' + z);
+					putchar('0' + j);
+					putchar('0' + i);
+
+					if (i + j + z != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				i++;
+			}
+			j++;
+		}
+		z++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
