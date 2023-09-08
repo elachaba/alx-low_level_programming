8ceccd06b9bda10a71f55e0dc297d9d630a736ca
Aymane <elachabaymane995@gmail.com>
2023-09-08 12:34:43 -0700

    hot fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(c);
-		if (c <= '8')
+		putchar('0' + i);
+		if (i <= 8)
 		{
 			putchar(',');
 			putchar(' ');
