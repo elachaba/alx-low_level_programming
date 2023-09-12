f0515e226c789b49c3167b3cce33e44955b447aa
Aymane <elachabaymane995@gmail.com>
2023-09-12 04:50:41 -0700

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
- *
- * Rerturn: 0
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
 int main(void)
 {
