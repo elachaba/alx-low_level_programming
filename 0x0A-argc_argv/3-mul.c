ec61277dd12b826e58fcca0f2cf9661c38e8b122
Aymane <elachabaymane995@gmail.com>
2023-10-01 23:53:44 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * _atoi - converts a string into an integer
+ * @s: the string to be converted
+ *
+ * Return: an integer represented in s
+ */
+
+int _atoi(char *s)
+{
+	int i, sign;
+
+	unsigned int res;
+
+	res = 0;
+	i = 0;
+	sign = 1;
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+	while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
+	{
+		res += (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
+}
+
+
+
+/**
+ * main - prints the multiplication of two numbers
+ * passed in the arguments
+ * @argc: number of arguments
+ * @argv: an array of pointers to the strings represeting te args
+ *
+ * Return: EXIT_SUCCESS if mult is done, else EXIT_FAILURE
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (EXIT_FAILURE);
+	}
+
+	a = _atoi(argv[1]);
+	b = _atoi(argv[2]);
+
+	printf("%d\n", a * b);
+	return (EXIT_SUCCESS);
+}
+
