f4c985c2b7093cf71d1a3714fbc793739cac1d6e
elachaba <elachabaymane995@gmail.com>
2023-10-19 15:40:04 -0700

    constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void first(void __attribute__(constructor));
+
+/**
+ * first - prints a long sentence the stdout
+ *
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
