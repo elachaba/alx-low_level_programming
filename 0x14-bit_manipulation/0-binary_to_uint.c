b19bd4c8159c3ef589c0cc719cedfe18527a907c
elachaba <elachabaymane995@gmail.com>
2023-10-26 17:27:57 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
 /**
  * binary_to_uint - converts a binary number into an usigned int
  * @b: a poinrer to the string of bits
- * 
+ *
  * Return: 0 if there's a chracter in b that is not 0 or 1 or if b is NULL
  * else the corresponding int
  */
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		return (res);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		res = res * 2 + (b[i] - '0');
 	}
