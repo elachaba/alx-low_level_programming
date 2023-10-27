dab5e6c1111a675da724e4475d84b85762752871
elachaba <elachabaymane995@gmail.com>
2023-10-27 16:00:20 -0700

    flipping bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * set_bit: sets the value of a bit to 1 on a given index
+ * @n: the decimal to manipulate
+ * @index: index
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int set_bit(unsigned int *n, unsinged int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n |= (1 << index);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * clear_bit - sets the value of a bit to 0 on a given index
+ * @n: the integer to modify
+ * @index: the index to modify
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int clear_bit(unsigned int *n, unsigned int index)
+{
+	if (index > sizeof(int) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
