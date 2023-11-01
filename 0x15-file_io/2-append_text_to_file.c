89a664607ab93407cb92379496b888a98ca65712
elachaba <elachabaymane995@gmail.com>
2023-11-01 16:22:19 -0700

    I dunno the difference

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,34 @@
 #include "main.h"
-#include <stdlib.h>
-
-#define SUCCESS 1
-#define FAILURE -1
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: the name of the file
- * @text_content: is a NULL terminated string to add at the end of the file
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
  *
- * Return 1 on success and -1 on failure
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int o, w, len = 0;
 
-	if (!filename)
-		return (FAILURE);
+	if (filename == NULL)
+		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	w = write(fd, text_content, len);
-	if (fd == -1 || w == -1)
-		return (FAILURE);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	close(fd);
-	return (SUCCESS);
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
