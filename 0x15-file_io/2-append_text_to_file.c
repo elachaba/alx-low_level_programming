c52212d9954a74a7fda3ba9cb3bd0827215e5276
elachaba <elachabaymane995@gmail.com>
2023-11-01 16:31:11 -0700

    cp fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (o == -1 || w == -1)
 		return (-1);
 
-	close(o);
+	close(fd);
 
 	return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,21 +1,26 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * allocate_buff - allocates the necessary memory for the buffer
- * @file: the name of the file to write to
+ * allocate_buff - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: the buffer
+ * Return: A pointer to the newly-allocated buffer.
  */
-
 char *allocate_buff(char *file)
 {
-	char *buffer = malloc(1024 * sizeof(char));
+	char *buffer;
 
-	if (!buffer)
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -23,15 +28,14 @@ char *allocate_buff(char *file)
 }
 
 /**
- * close_checked - closes a file and checks is it was closed correctly
- * @fd: the index of the file in the file descriptor table
- *
- * Return: void
+ * close_checked - Closes file descriptors and checks if they close correctly
+ * @fd: The file descriptor to be closed.
  */
-
 void close_checked(int fd)
 {
-	int c = close(fd);
+	int c;
+
+	c = close(fd);
 
 	if (c == -1)
 	{
@@ -40,23 +44,21 @@ void close_checked(int fd)
 	}
 }
 
-
 /**
  * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program
- * @argv: a list of pointers to the arguments of the program
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success
+ * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97
- *		If file_from oes not exist or cannot be read - exit code 98
- *		If file_to cannot be created or written to - exit code 99
- *		If file_to or file_from cannot be closed - exit 100
+ * Description: If the argument count is incorrect - exit code 97.
+ *		If file_from does not exist or cannot be read - exit code 98.
+ *		If file_to cannot be created or written to - exit code 99.
+ *		If file_to or file_from cannot be closed - exit code 100.
  */
-
 int main(int argc, char *argv[])
 {
-	int src, dest, r, w;
+	int src, dst, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -64,10 +66,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	buffer = allocate_buff(argv[2]);
 	src = open(argv[1], O_RDONLY);
 	r = read(src, buffer, 1024);
-	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	dst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (src == -1 || r == -1)
@@ -75,23 +78,26 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
-			exit(99);
+			exit(98);
 		}
-		w = write(dest, buffer, r);
-		if (dest == -1 || w == -1)
+
+		w = write(dst, buffer, r);
+		if (dst == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to file %s\n", argv[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
+
 		r = read(src, buffer, 1024);
-		dest = open(argv[2], O_WRONLY | O_APPEND);
+		dst = open(argv[2], O_WRONLY | O_APPEND);
+
 	} while (r > 0);
 
 	free(buffer);
 	close_checked(src);
-	close_checked(dest);
+	close_checked(dst);
 
 	return (0);
 }
